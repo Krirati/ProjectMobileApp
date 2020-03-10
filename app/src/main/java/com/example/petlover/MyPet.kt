8447c580c2a48961fbecfbb839c85e982ed43d08
krirati <kriratik@kkumail.com>
2020-03-10 14:33:45 +0700

    update addpet

--- app/src/main/java/com/example/petlover/MyPet.kt
+++ app/src/main/java/com/example/petlover/MyPet.kt
@@ -1,8 +1,11 @@
 package com.example.petlover
 
+import com.google.type.LatLng
+
 data class MyPet (
     var name: String = "",
     var pedigree: String = "",
     var gender: String = "",
-    var birthday: String = ""
+    var birthday: String = "",
+    var location: String? = ""
 )
